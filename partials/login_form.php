cc492798e9d8af5cc753dc540262ddb51ef35f43
Philip-11 <magsaysayphil@gmail.com>
2024-11-15 13:33:38 +0800

    Added login function to dashboard

--- partials/login_form.php
+++ partials/login_form.php
@@ -1,3 +1,23 @@
+<?php 
+    include "../includes/db.php";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+
+        $sql = "SELECT * FROM profile_students WHERE email='$email'";
+        $query = $conn->query($sql);
+
+        if ($query->num_rows > 0){
+            $user = $query->fetch_assoc();
+
+            if ($password == $user["password"]){
+                header("Location: ../pages/dashboard.php");
+            }
+        }
+    }
+?>
+
 <form action="" method="post">
     <input type="email" name="email" id="email" placeholder="Email Address" required> <br>
     <input type="password" name="password" id="password" placeholder="Password" required> <br>
